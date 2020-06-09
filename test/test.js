7ce443d1541764fb55a5cb6e6875f5096ef93578
Rakesh <bhattrakesh09@gmail.com>
2020-06-09 13:27:08 +0530

    Testing nodejs app changes

--- test/test.js
+++ test/test.js
@@ -6,15 +6,15 @@ const server = request('http://localhost:3000');
 
 describe("Sample unit test", () => {
 
-    it('Should return home page ', () => {
+    it('Should return home page ', (done) => {
+
         server.get("/")
         .expect('Content-type', /text/)
         .expect(200)
         .end((err, res) =>  {
-
-            console.log("Error : ", err);         
-            res.status.should.equal(200);
+            console.log("Error : ", res.status);         
+            expect(res.status.should.equal(200));
+            done();
         });
-   
     });
 });
